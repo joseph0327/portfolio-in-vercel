391e1b95e037263e2c18e9278600101075f1245d
joseph0327 <joseph.villarin0327@gmail.com>
2024-12-07 13:39:08 +0800

    first commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -89,9 +89,10 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
+
 
 
 # Password validation
